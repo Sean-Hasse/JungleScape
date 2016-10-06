6738a03b8c08bc98111181f47158c8d7f95dd0ac
Max <Max@QUAGMELONIII>
2016-10-06 16:26:18 -0400

    Gamestate switching is up. Just shows debug messages for now.

--- JungleScape/JungleScape/Game1.cs
+++ JungleScape/JungleScape/Game1.cs
@@ -7,6 +7,17 @@ using System.Linq;
 namespace JungleScape
 {
     /// <summary>
+    /// Available GameStates
+    /// </summary>
+    enum GameState
+    {
+        Menu,
+        Instructions,
+        Game,
+        Pause,
+        GameOver
+    };
+    /// <summary>
     /// This is the main type for your game.
     /// </summary>
     public class Game1 : Game
@@ -20,7 +31,12 @@ namespace JungleScape
 
         Map levelMap;
         List<Texture2D> textures;
-
+        GameState myState;
+        int menuIndex;
+        KeyboardState previousKbState;
+        KeyboardState kbState;
+        SpriteFont testFont;
+        
 
         public Game1()
         {
@@ -39,6 +55,8 @@ namespace JungleScape
             // TODO: Add your initialization logic here
             levelMap = new Map();
             textures = new List<Texture2D>();
+            myState = GameState.Menu;
+            menuIndex = 0;
 
             base.Initialize();
         }
@@ -62,6 +80,8 @@ namespace JungleScape
             textures.Add(Content.Load<Texture2D>("firstPlatformerBrick"));
             textures.Add(Content.Load<Texture2D>("BasicPlayer"));
 
+            testFont = Content.Load<SpriteFont>("testFont");
+
             levelMap.loadMap(textures);
         }
 
@@ -81,15 +101,69 @@ namespace JungleScape
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
 
             // TODO: Add your update logic here
-
-            foreach (Character chara in levelMap.objectMap.OfType<Character>())
+            
+            previousKbState = kbState;
+            kbState = Keyboard.GetState();
+            switch (myState)
             {
-                chara.Move();
+                case GameState.Menu:
+                    if (SingleKeyPress(Keys.Down, kbState, previousKbState))
+                        menuIndex += 1;
+                    else if (SingleKeyPress(Keys.Up, kbState, previousKbState))
+                        menuIndex -= 1;
+
+                    if (menuIndex >= 3)
+                        menuIndex = 0;
+                    else if (menuIndex < 0)
+                        menuIndex = 2;
+
+                    if (SingleKeyPress(Keys.Enter, kbState, previousKbState) && menuIndex == 0)
+                        myState = GameState.Game;
+                    else if (SingleKeyPress(Keys.Enter, kbState, previousKbState) && menuIndex == 1)
+                        myState = GameState.Instructions;
+                    else if (Keyboard.GetState().IsKeyDown(Keys.Enter) && menuIndex == 2)
+                        Exit();
+
+                    break;
+
+                case GameState.Instructions:
+                    if (SingleKeyPress(Keys.Enter, kbState, previousKbState) || SingleKeyPress(Keys.Back, kbState, previousKbState))
+                        myState = GameState.Menu;
+                    break;
+
+                case GameState.Game:
+                    foreach (Character chara in levelMap.objectMap.OfType<Character>())
+                    {
+                    chara.Move();
+                    }
+
+                    if (SingleKeyPress(Keys.P, kbState, previousKbState) || SingleKeyPress(Keys.Escape, kbState, previousKbState))
+                        myState = GameState.Pause;
+                    else if (Keyboard.GetState().IsKeyDown(Keys.G))
+                        myState = GameState.GameOver;
+                    break;
+
+                case GameState.Pause:
+                    if (SingleKeyPress(Keys.P, kbState, previousKbState) || SingleKeyPress(Keys.Escape, kbState, previousKbState))
+                        myState = GameState.Game;
+                    break;
+
+                case GameState.GameOver:
+                    if (SingleKeyPress(Keys.Enter, kbState, previousKbState))
+                    {
+                        myState = GameState.Menu;
+                        menuIndex = 0;
+                    }
+                       
+                    break;
+
+                default:
+                    break;
             }
+
+            
             
             base.Update(gameTime);
         }
@@ -103,12 +177,55 @@ namespace JungleScape
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
             spriteBatch.Begin();
-            levelMap.drawMap(spriteBatch);
+
+            switch (myState)
+            {
+                case GameState.Menu:
+                    spriteBatch.DrawString(testFont, "This is a Menu", new Vector2(0, 0), Color.White);
+                    if (menuIndex == 0)
+                        spriteBatch.DrawString(testFont, "Start Game is selected", new Vector2(0, 50), Color.White);
+                    else if (menuIndex == 1)
+                        spriteBatch.DrawString(testFont, "Instructions are selected", new Vector2(0, 50), Color.White);
+                    else
+                        spriteBatch.DrawString(testFont, "exit game is selected", new Vector2(0, 50), Color.White);
+                    break;
+
+                case GameState.Instructions:
+                    spriteBatch.DrawString(testFont, "These are Instructions", new Vector2(0, 0), Color.White);
+                    break;
+
+                case GameState.Game:
+                    levelMap.drawMap(spriteBatch);
+                    spriteBatch.DrawString(testFont, "This is a Game Screen", new Vector2(0, 0), Color.White);
+                    break;
+
+                case GameState.Pause:
+                    spriteBatch.DrawString(testFont, "This is a pause menu", new Vector2(0, 0), Color.White);
+                    break;
+
+                case GameState.GameOver:
+                    spriteBatch.DrawString(testFont, "Game over man, game over.", new Vector2(0, 0), Color.White);
+                    break;
+
+                default:
+                    break;
+            }
+            
             spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+        public bool SingleKeyPress(Keys key, KeyboardState kbState, KeyboardState previousKbState)
+        {
+            if (kbState.IsKeyDown(key) && previousKbState.IsKeyUp(key))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
