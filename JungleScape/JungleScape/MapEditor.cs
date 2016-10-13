860d3f9ecf8e4a78983a8ce9ccecfdfb3f93070b
Brady Murren <bsm4978@g.rit.edu>
2016-10-13 13:31:44 -0400

    removed leveleditor project and moved windform into main game project

---
+++ JungleScape/JungleScape/MapEditor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LevelEditor
+{
+    public partial class MapEditor : Form
+    {
+        public MapEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void MapEditor_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Height_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
