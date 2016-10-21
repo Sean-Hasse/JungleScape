e253634f46c776b981b5f85c5c1346e91219d84a
ab4854 <ab4854@IMM-3>
2016-10-21 15:36:52 -0400

    Grammar storyline fix.

--- JungleScape/JungleScape/Game1.cs
+++ JungleScape/JungleScape/Game1.cs
@@ -342,13 +342,13 @@ namespace JungleScape
                     break;
 
                 case GameState.Story:
-                    spriteBatch.DrawString(testFont, "JungleScape Storyline", new Vector2(0, 0), Color.White);
+                    spriteBatch.DrawString(testFont, "JungleScape Storyline:", new Vector2(0, 0), Color.White);
                     spriteBatch.DrawString(testFont, "The player was going to a professional archery competition in South America.", new Vector2(0, 100), Color.White);
                     spriteBatch.DrawString(testFont, "Suddenly his plane crashes in the middle of wild, unexplored jungle.", new Vector2(0, 150), Color.White);
                     spriteBatch.DrawString(testFont, "The player has to survive huge bugs and wild animals.", new Vector2(0, 200), Color.White);
                     spriteBatch.DrawString(testFont, "All while solving puzzles to find his way out.", new Vector2(0, 250), Color.White);
                     spriteBatch.DrawString(testFont, "Creators:", new Vector2(0, 350), Color.White);
-                    spriteBatch.DrawString(testFont, "Project Manager: Alexia Bernardo:", new Vector2(0, 400), Color.White);
+                    spriteBatch.DrawString(testFont, "Project Manager: Alexia Bernardo", new Vector2(0, 400), Color.White);
                     spriteBatch.DrawString(testFont, "Architect: Sean Hasse", new Vector2(0, 450), Color.White);
                     spriteBatch.DrawString(testFont, "Game Designer: Brady Murren", new Vector2(0, 500), Color.White);
                     spriteBatch.DrawString(testFont, "Interface Designer: Max Kaiser", new Vector2(0, 550), Color.White);
