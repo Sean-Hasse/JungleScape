c9867f4f2fe942be180e972771118b0d77981881
Brady Murren <bsm4978@g.rit.edu>
2016-10-22 22:00:48 -0400

    Deleted unused classes.  Started on Boss.

---
+++ JungleScape/JungleScape/Boss.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace JungleScape
+{
+    public class Boss : Character
+    {
+        public Boss(Rectangle hBox, Texture2D texture) : base(hBox, texture)
+        {
+            speedX = 4;
+        }
+
+        public override void Move()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Move(List<GameObject> gObj)
+        {
+            foreach(Player p in gObj.OfType<Player>())
+            {
+                if (p.hitBox.X > hitBox.X)
+                    hitBox.X += speedX;
+                else if (p.hitBox.X < hitBox.X)
+                    hitBox.X -= speedX;
+            }
+            
+            
+        }
+    }
+}
