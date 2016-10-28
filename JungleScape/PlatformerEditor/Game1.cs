113ee21a1cccb5646307d3a4f5ec9d18de4568c4
ab4854 <ab4854@IMM-3>
2016-10-28 16:13:13 -0400

    Got rid of in screen game instrcutions, and made bounds around the background black instead of cornblue.

--- JungleScape/PlatformerEditor/Game1.cs
+++ JungleScape/PlatformerEditor/Game1.cs
@@ -230,7 +230,7 @@ namespace PlatformerEditor
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
             //spriteBatch.Begin();
