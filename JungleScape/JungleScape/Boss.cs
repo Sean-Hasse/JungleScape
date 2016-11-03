ea76c8dd10ab7ac2543ebe433e2dd7c5503fbc39
Sean Hasse <sxh6213@rit.edu>
2016-11-03 13:40:46 -0400

    Working on getting the Boss Pounce() method working

--- JungleScape/JungleScape/Boss.cs
+++ JungleScape/JungleScape/Boss.cs
@@ -12,13 +12,17 @@ namespace JungleScape
     {
         // attributes
         List<GameObject> gameObjs;
+        List<BossLeapZone> leapList;
         BossLeapZone currentZone;
+        double leapTimer;
 
-        public Boss(Rectangle hBox, List<GameObject> env, Texture2D texture, int hp, BossLeapZone startZone) : base(hBox, env, texture, hp)
+        public Boss(Rectangle hBox, List<GameObject> env, Texture2D texture, int hp, BossLeapZone startZone, List<BossLeapZone> lList) : base(hBox, env, texture, hp)
         {
             currentZone = startZone;
+            leapList = lList;
             gameObjs = env;
             speedX = 4;
+            leapTimer = 0;
         }
 
         // methods
@@ -33,6 +37,20 @@ namespace JungleScape
                 else if (p.hitBox.X < hitBox.X)
                     hitBox.X -= speedX;
             }
+
+            // update the timer
+            leapTimer++;
+
+            if (leapTimer >= 30)
+                Pounce();
+        }
+
+        // method for boss to move from platoform to platform towards the Player. 
+        private void Pounce()
+        {
+            int listID = currentZone.id;
+            List<BossLeapZone> jumpZones = new List<BossLeapZone>();
+
         }
 
         // other Move method. Not implemented here
