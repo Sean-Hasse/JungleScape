70dbd670238e675c3e39d45004868fde4c4c8634
mrk5790 <mrk5790@GDD-36>
2016-11-03 13:59:21 -0400

    fixed aiming overrides. Player's health is drawn to screen.

--- JungleScape/JungleScape/Map.cs
+++ JungleScape/JungleScape/Map.cs
@@ -70,7 +70,14 @@ namespace JungleScape
                 //If map object is the player, it takes the keyboard state and draws where the player is currently aiming
                 if (obj is Player)
                 {
-                    if (kbState.IsKeyDown(Keys.Right) && kbState.IsKeyDown(Keys.A))
+                    
+                    if (kbState.IsKeyDown(Keys.A) && kbState.IsKeyDown(Keys.Up) && kbState.IsKeyDown(Keys.Right))
+                        spriteBatch.Draw(textures[1], obj.hitBox, null, Color.White, 0, Vector2.Zero, SpriteEffects.None, 0f);
+                    else if (kbState.IsKeyDown(Keys.A) && kbState.IsKeyDown(Keys.Up) && kbState.IsKeyDown(Keys.Left))
+                        spriteBatch.Draw(textures[1], obj.hitBox, null, Color.White, 0, Vector2.Zero, SpriteEffects.FlipHorizontally, 0f);
+                    else if (kbState.IsKeyDown(Keys.A) && kbState.IsKeyDown(Keys.Up))
+                        spriteBatch.Draw(textures[2], obj.hitBox, null, Color.White, 0, Vector2.Zero, SpriteEffects.None, 0f);
+                    else if (kbState.IsKeyDown(Keys.Right) && kbState.IsKeyDown(Keys.A))
                         spriteBatch.Draw(textures[0], obj.hitBox, null, Color.White, 0, Vector2.Zero, SpriteEffects.None, 0f);
                     else if (kbState.IsKeyDown(Keys.Left) && kbState.IsKeyDown(Keys.Up))
                         spriteBatch.Draw(textures[1], obj.hitBox, null, Color.White, 0, Vector2.Zero, SpriteEffects.FlipHorizontally, 0f);
